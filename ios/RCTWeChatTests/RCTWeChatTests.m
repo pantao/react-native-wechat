824773c2b5ce7e26c5f34346a4c37a6b136abf7c
pantao <54778899@qq.com>
2018-07-06 12:01:32 +0800

    UPDATE

---
+++ ios/RCTWeChatTests/RCTWeChatTests.m
@@ -0,0 +1,39 @@
+//
+//  RCTWeChatTests.m
+//  RCTWeChatTests
+//
+//  Created by pantao 18-07-06.
+//  Copyright © 2018 pantao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RCTWeChatTests : XCTestCase
+
+@end
+
+@implementation RCTWeChatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
